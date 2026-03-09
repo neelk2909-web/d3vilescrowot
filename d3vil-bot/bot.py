11a9ac4300be0d8ea8dcfa1a21d45802be3ad350
neelk2909-web <neelk2909@gmail.com>
2026-03-09 12:09:42 +0530

    Update bot.py

--- d3vil-bot/bot.py
+++ d3vil-bot/bot.py
@@ -4,8 +4,27 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = "8606167168:AAGiUyxVwHjMkOqgZHOY61XBN8phc_4l_Mg"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Welcome to D3VIL ESCROWS")
+await update.message.reply_text("""
+💫 @d3vil_escrowbot 💫
+Your Trustworthy Telegram Escrow Service
 
+Welcome to @d3vil_escrowbot. This bot provides a reliable escrow service for your transactions on Telegram.
+
+Avoid scams, your funds are safeguarded throughout your deals. If you run into any issues, simply type /dispute and an arbitrator will join the group chat within 24 hours.
+
+🎟 ESCROW FEE:
+1.0% for P2P and 1.0% for OTC Flat
+
+🌐 UPDATES & VOUCHES:
+@thed3vilnetwork
+
+💬 Proceed with /escrow (to start with a new escrow)
+
+⚠️ IMPORTANT:
+Make sure coin is same of Buyer and Seller else you may lose your coin.
+
+💡 Type /menu to summon a menu with all bots features
+""")
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
